1e75544dc1f8a467aca41403aa0e8c312a431573
abrackx <abrackx@gmail.com>
2023-12-31 09:50:50 -0500

    Use react router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {ChangeEvent, useEffect, useState} from "react"
+import {createContext, useContext, useEffect, useState} from "react"
 import OBSWebSocket from "obs-websocket-js";
 import setupObsWebSocket from "./obs.tsx";
+import {createHashRouter, RouterProvider, useNavigate} from "react-router-dom";
 
 type ObsContextType = {
     url?: string,
@@ -15,68 +16,75 @@ const defaultObsContext = {
     }
 };
 
-const ObsContext = React.createContext(defaultObsContext);
+const ObsContext = createContext(defaultObsContext);
 
 const LoginForm = () => {
-    const {data, setData} = React.useContext(ObsContext);
-    const {url} = data
-    useEffect(() => {
-        const callIt = async (url: string) => {
-            return await setupObsWebSocket(url, password.value);
-        }
-        if (url && url !== defaultObsContext.data.url) {
-            const obs = callIt(url);
+    const {setData} = useContext(ObsContext);
+    const navigate = useNavigate();
+    const [form, setForm] = useState({
+        url: '',
+        password: '',
+    });
+
+    const handleChange = (event: { target: { id: any; value: any; }; }) => {
+        setForm({
+            ...form,
+            [event.target.id]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event: { preventDefault: () => void; }) => {
+        event.preventDefault();
+        if (form.url && form.url !== defaultObsContext.data.url) {
+            const obs = setupObsWebSocket(form.url, form.password);
             obs.then(connection => {
                 if (connection) {
-                    setData({obs: connection, ...data})
+                    setData({obs: connection, url: form.url})
+                    navigate('/home', {replace: true})
                 }
             })
         }
-    }, [url])
+    };
 
-
-    const [visibleUrl, setVisibleUrl] = useState({value: url});
-    const [password, setPassword] = useState({value: ""});
-
-    if (data.obs !== undefined) {
-        return <></>
-    }
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <div>
-                <input onChange={(event: ChangeEvent<{ value: string }>) => {
-                    setVisibleUrl({value: event?.currentTarget?.value});
-                }} value={visibleUrl.value}/>
-                <input onChange={(event: ChangeEvent<{ value: string }>) => {
-                    setPassword({value: event?.currentTarget?.value});
-                }} value={password.value} type="password"/>
+                <label htmlFor="url">url</label>
+                <input
+                    id="url"
+                    type="text"
+                    value={form.url}
+                    onChange={handleChange}
+                />
             </div>
-            <button onClick={() => {
-                setData({url: visibleUrl?.value})
-            }}>
-                Log in!
-            </button>
-        </div>
-    )
+            <div>
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    type="password"
+                    value={form.password}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
 }
 
 const Main = () => {
-    const {data} = React.useContext(ObsContext);
+    const {data} = useContext(ObsContext);
     const [version, setVersion] = useState("")
     const obs = data.obs;
     useEffect(() => {
-        const callIt = async () => {
-            return obs?.call("GetVersion");
-        }
-        callIt().then(maybeVersion => {
-            if (maybeVersion) {
-                setVersion(maybeVersion.obsVersion)
+        (async () => {
+            try {
+                const d = await obs?.call("GetVersion")
+                setVersion(d?.obsVersion || "")
+            } catch (e) {
+                console.error(e)
             }
-        })
+        })();
     }, [data.obs]);
-    if (!data.obs) {
-        return <></>
-    }
     return (
         <div>
             We did it! Connected to OBS Version: {version}
@@ -84,14 +92,24 @@ const Main = () => {
     )
 }
 
+const router = createHashRouter([
+    {
+        path: "/",
+        element: <LoginForm/>,
+    },
+    {
+        path: "/home",
+        element: <Main/>,
+    },
+]);
+
 export default function App() {
-    const [data, setData] = React.useState(defaultObsContext.data)
+    const [data, setData] = useState(defaultObsContext.data)
     const changeValues = (value: ObsContextType) => setData(data && value);
 
     return (
         <ObsContext.Provider value={{data, setData: changeValues}}>
-            <LoginForm/>
-            <Main/>
+            <RouterProvider router={router}/>
         </ObsContext.Provider>
     )
 }
